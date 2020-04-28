510015637d5981905050433e66749f2a8216f001
John Stevens <john.william.stevens1@gmail.com>
2020-04-28 10:46:18 -0700

    Fixed missing slashes identified by Ryan

--- manager_app/urls.py
+++ manager_app/urls.py
@@ -6,12 +6,12 @@ urlpatterns = [
         # GET -> render dashboard.html
         # POST -> handles Create Contract/Customer/Carrier
 
-    path("carrier/<int:carrier_id>", views.carrier),
+    path("/carrier/<int:carrier_id>", views.carrier),
         # GET -> render carrier.html
 
-    path("customer/<int:customer_id>", views.customer),
+    path("/customer/<int:customer_id>", views.customer),
         # GET -> render customer.html
 
-    path("contract/<int:contract_id>", views.contract),
+    path("/contract/<int:contract_id>", views.contract),
         # GET -> render contract.html
 ]
