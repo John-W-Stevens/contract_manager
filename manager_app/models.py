bbe72fde448c7a9c4123c803d9b9c2e019acea5f
John Stevens <john.william.stevens1@gmail.com>
2020-04-29 08:32:34 -0700

    Added trip_number attr to contract class, changed contract create form to accept trip_number input and displayed trip_number on contracts table

--- manager_app/models.py
+++ manager_app/models.py
@@ -36,6 +36,7 @@ class Customer(models.Model):
     updated_at= models.DateTimeField(auto_now=True)
 
 class Contract(models.Model):
+    trip_number= models.CharField(max_length=45, null=True)
     archived= models.BooleanField(default=False)
     status= models.CharField(max_length=255)
     carrier_cost= models.DecimalField(decimal_places= 2, max_digits=8,  null=True)
